5ef1caa5deaa8e147b505807cd8660b138b3baf0
vuphamngocthanh <protonmailvideo@gmail.com>
2023-02-28 16:32:56 -0800

    first commit

---
+++ src/main/java/com/lovetolearn/SpringBootApp/SpringBootAppApplication.java
@@ -0,0 +1,22 @@
+package com.lovetolearn.SpringBootApp;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootAppApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(SpringBootAppApplication.class);
+	  }
+}
